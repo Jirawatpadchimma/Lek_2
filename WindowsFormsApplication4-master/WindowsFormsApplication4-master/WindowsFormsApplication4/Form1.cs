e01c9f701ed08a755a7a6c2c948c0f92b958a43a
mookmanee <pathomphob_toey@hotmail.com>
2013-11-13 11:39:04 +0700

    Work_2
    Form1 to Form2

---
+++ WindowsFormsApplication4-master/WindowsFormsApplication4-master/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication4;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCheck_Click(object sender, EventArgs e)
+        {
+            string str;
+            string n;
+            string m;
+
+
+            
+
+            
+            if (bpt1.Checked == true)
+            {
+                str =  "โรงเรียน" ;
+            }
+            else if (bpt2.Checked == true)
+            {
+                str = "เทศบาล";
+            }
+            else  if (radioButton1.Checked == true)
+            {
+                str =  "อบจ." ;
+
+            }
+            else   if (radioButton3.Checked == true)
+            {
+                str =  "อบต." ;
+            }
+            else   if (radioButton8.Checked == true)
+            {
+                str =  "กศน.อำเภอ" ;
+            }
+            else  if (radioButton2.Checked == true)
+            {
+                str =  "กศน.จังหวัด" ;
+            }
+            else if (radioButton9.Checked == true)
+            {
+                if (textBox1.Text == "")
+                {
+                    str = "กรุณากรอกข้อมูล";
+                }
+                else
+                {
+                    str = "อื่น ๆ : "+ textBox1 .Text ;
+                }
+            }else 
+            {
+                str = "โรงเรียน";
+            }
+            {
+            }
+           // str = str + "2.ท่านได้รับข่าวสารข้อมูลจากแหล่งใด ===";
+            if (cpt1.Checked == true)
+            {
+                n =  "www.google.co.th" ;
+            }
+            else  if (cpt2.Checked == true)
+            {
+                n = "www.yahoo.com" ;
+            }
+            else   if (cpt3.Checked == true)
+            {
+               n= "แผ่นพับประชาสัมพันธ์ที่ได้รับแจก"  ;
+            }
+            else  if (radioButton4.Checked == true)
+            {
+                n= "แผ่นซีดีข้อมูลที่ได้รับแจก"  ;
+            }
+            else   if (radioButton10.Checked == true)
+            {
+              n= "การบอกต่อ"  ;
+            }
+            else  if (radioButton11.Checked == true)
+            {
+                if (textBox2.Text == "")
+                {
+                    n = "กรุณากรอกข้อมูล";
+                }
+                else
+                {
+                    n = "อื่น ๆ : " + textBox2.Text;
+                }
+            }
+                else
+            {
+                {
+                    n = "www.google.co.th";
+                }
+
+            }
+           // str = str + "3.แหล่งเรียนรู้และภูมิท้องถิ่นไปใช้ประโยชน์อย่างไรบ้าง ===";
+            if (dpt1.Checked == true)
+            {
+                m= "จัดการเรียนการสอน" ;
+            }
+            else   if (dpt2.Checked == true)
+            {
+                m= "ศึกษาค้นคว้าและทำรายงาน"  ;
+            }
+            else  if (dpt3.Checked == true)
+            {
+                m= "ข้อมูลเชิงการท่องเที่ยว"  ;
+            }
+            else if (radioButton5.Checked == true)
+            {
+                if (textBox3.Text == "")
+                {
+                    m = "กรุณากรอกข้อมูล";
+                }
+                else
+                {
+                    m = "อื่น ๆ : " + textBox3.Text;
+                }
+            }
+            else
+            {
+                m = "จัดการเรียนการสอน";
+            }
+            {   
+           
+
+
+
+
+
+
+
+
+
+            }
+            Form2 frm = new Form2(str ,n ,m );
+            frm.Show();
+        }
+
+        private void optMale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apt2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ WindowsFormsApplication4-master/WindowsFormsApplication4-master/WindowsFormsApplication4/Form2.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(526, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.Color.Cyan;
+            this.label2.Location = new System.Drawing.Point(526, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(526, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(32, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(485, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "3.แหล่งเรียนรู้และภูมิท้องถิ่นไปใช้ประโยชน์อย่างไรบ้าง";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(32, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(337, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "2.ท่านได้รับข่าวสารข้อมูลจากแหล่งใด";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label6.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label6.Location = new System.Drawing.Point(32, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "1.หน่วยงานที่สังกัด";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 264);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
